a4cb464b4f31ed460322e805f7cf1165721ba2a3
Erik David Avila Cruz <davila_2496@hotmail.com>
2025-11-06 23:59:57 -0600

    Updated login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,47 +1,8 @@
+// src/app/login/login.component.ts (temporal)
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
-import { AuthService } from '../services/auth.service';
 
 @Component({
-  selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  template: `<h1 style="padding:2rem">Login OK</h1>`
 })
-export class LoginComponent {
-  username: string = '';
-  password: string = '';
-  errorMessage: string = '';
-  isLoading: boolean = false;
-  showPassword: boolean = false;
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  onSubmit(): void {
-    this.errorMessage = '';
-
-    if (!this.username || !this.password) {
-      this.errorMessage = 'Usuario y contraseña son requeridos';
-      return;
-    }
-
-    this.isLoading = true;
-
-    this.authService.login(this.username, this.password)
-      .then(() => {
-        this.router.navigate(['/dashboard']);
-      })
-      .catch(error => {
-        this.errorMessage = error.message;
-        this.password = '';
-        this.isLoading = false;
-      });
-  }
-
-  togglePasswordVisibility(): void {
-    this.showPassword = !this.showPassword;
-  }
-}
+export class LoginComponent {}
