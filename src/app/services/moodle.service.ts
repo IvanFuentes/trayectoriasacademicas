ad433502e8c8805f802e7eb545b5d3888f70013b
Fuentes-Chab, Iván Humberto <ivanfuenteschab@gmail.com>
2025-11-10 02:41:41 -0600

    Updated moodle.service.ts

--- src/app/services/moodle.service.ts
+++ src/app/services/moodle.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, catchError } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 export interface Carrera {
   id: number;
@@ -42,60 +41,82 @@ export interface EstudianteFaltas {
   providedIn: 'root'
 })
 export class MoodleService {
-  private apiUrl = '/functions/v1/moodle-data';
+  private supabaseUrl = 'https://yxuzeeblexqpjfmnfxhy.supabase.co';
+  private supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4dXplZWJsZXhxcGpmbW5meGh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NDA0MzcsImV4cCI6MjA3ODMxNjQzN30.3ljtL7SzH-M0vDsQn-FgvdWyS1Ze-Mqzn2pnVFunsIY';
+  private apiUrl = `${this.supabaseUrl}/functions/v1/moodle-data`;
 
   constructor(private http: HttpClient) {}
 
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.supabaseKey}`,
+      'apikey': this.supabaseKey
+    });
+  }
+
   getCarreras(): Observable<Carrera[]> {
-    return this.http.get<Carrera[]>(`${this.apiUrl}?action=carreras`).pipe(
+    return this.http.get<Carrera[]>(`${this.apiUrl}?action=carreras`, {
+      headers: this.getHeaders()
+    }).pipe(
       catchError(error => {
         console.error('Error fetching carreras:', error);
-        throw error;
+        return throwError(() => error);
       })
     );
   }
 
   getCursos(carreraId: number): Observable<Curso[]> {
-    return this.http.get<Curso[]>(`${this.apiUrl}?action=cursos&carrera_id=${carreraId}`).pipe(
+    return this.http.get<Curso[]>(`${this.apiUrl}?action=cursos&carrera_id=${carreraId}`, {
+      headers: this.getHeaders()
+    }).pipe(
       catchError(error => {
         console.error('Error fetching cursos:', error);
-        throw error;
+        return throwError(() => error);
       })
     );
   }
 
   getSesionesAsistencia(carreraId: number): Observable<SesionAsistencia[]> {
-    return this.http.get<SesionAsistencia[]>(`${this.apiUrl}?action=sesiones-asistencia&carrera_id=${carreraId}`).pipe(
+    return this.http.get<SesionAsistencia[]>(`${this.apiUrl}?action=sesiones-asistencia&carrera_id=${carreraId}`, {
+      headers: this.getHeaders()
+    }).pipe(
       catchError(error => {
         console.error('Error fetching sesiones:', error);
-        throw error;
+        return throwError(() => error);
       })
     );
   }
 
   getEstudiantesFaltas(carreraId: number): Observable<EstudianteFaltas[]> {
-    return this.http.get<EstudianteFaltas[]>(`${this.apiUrl}?action=estudiantes-faltas&carrera_id=${carreraId}`).pipe(
+    return this.http.get<EstudianteFaltas[]>(`${this.apiUrl}?action=estudiantes-faltas&carrera_id=${carreraId}`, {
+      headers: this.getHeaders()
+    }).pipe(
       catchError(error => {
         console.error('Error fetching estudiantes con faltas:', error);
-        throw error;
+        return throwError(() => error);
       })
     );
   }
 
   getDocentes(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}?action=docentes`).pipe(
+    return this.http.get<any[]>(`${this.apiUrl}?action=docentes`, {
+      headers: this.getHeaders()
+    }).pipe(
       catchError(error => {
         console.error('Error fetching docentes:', error);
-        throw error;
+        return throwError(() => error);
       })
     );
   }
 
   getAsistenciasConfig(cursoId: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}?action=asistencias-config&curso_id=${cursoId}`).pipe(
+    return this.http.get<any>(`${this.apiUrl}?action=asistencias-config&curso_id=${cursoId}`, {
+      headers: this.getHeaders()
+    }).pipe(
       catchError(error => {
         console.error('Error checking attendance config:', error);
-        throw error;
+        return throwError(() => error);
       })
     );
   }
