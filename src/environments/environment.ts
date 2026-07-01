c5803b83fb67352ae9f9a07b8aacb78937e39e0a
Hesiquio Zarate Landa <zarate@itsescarcega.edu.mx>
2026-07-01 12:35:34 -0600

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  supabaseUrl: "https://bfajoeeskpbbccznofht.supabase.co",
-  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFub24iLCJpYXQiOjE3ODI4MzA3MDgsImV4cCI6MjA5ODQwNjcwOH0.65-1Hs_0TLSYlXpadBMMqMfU5V26qFz3FUB6MtpXshs"
+  supabaseUrl: "https://nlzqwpspabprxxbxqmou.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5senF3cHNwYWJwcnh4YnhxbW91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NzMwMzMsImV4cCI6MjA3ODA0OTAzM30.aMNYnnZNORDQFB4QTOdFy6fTniDBsRr4UUHiKVDdiVg"
 };
